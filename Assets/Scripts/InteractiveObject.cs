37ca33c6ef3b390f4439b9c73f0a8a9481aa6bbd
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2020-05-25 12:32:06 -0400

    super basic level stuff

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OFR.Psycho.Utils;
+using UnityEditor;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour {
+  // NOTE: place holder
+  private MeshRenderer renderer;
+  private Rigidbody rigidbody;
+  private Camera mainCam;
+
+  [SerializeField]
+  private Material defaultMat;
+  [SerializeField]
+  private Material telekinesisMat;
+
+  private bool interactable = true;
+
+  private void Awake() {
+    mainCam = Camera.main;
+    renderer = GetComponent<MeshRenderer>();
+    rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void OnMouseEnter() {
+    if (interactable)
+      renderer.material = telekinesisMat;
+  }
+
+  void OnMouseExit() {
+    renderer.material = defaultMat;
+  }
+
+  private void OnMouseDown() {
+    if (interactable)
+      Interact();
+  }
+
+  private void OnMouseUp() {
+    EndInteract();
+  }
+
+  void Interact() {
+    interactable = false;
+    rigidbody.isKinematic = false;
+  }
+
+  void EndInteract() {}
+}
