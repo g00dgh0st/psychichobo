7fcf6a3b225b4d5a8b193db8f709b28fbe2d14be
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2020-05-29 11:08:57 -0400

    more progress on sewer level

---
+++ Assets/Scripts/LightOscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOscillator : MonoBehaviour {
+
+  [SerializeField]
+  private float startDelay = 1f;
+  [SerializeField]
+  private float oscillateTime = 1f;
+
+  private Light light;
+  private float intensity;
+
+  void Awake() {
+    light = GetComponent<Light>();
+  }
+
+  void Start() {
+    intensity = light.intensity;
+  }
+
+  void Update() {
+
+  }
+
+}
